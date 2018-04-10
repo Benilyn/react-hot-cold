26219251b6859be87bb2aae944c72e587fb6aaa2
Benilyn <benilyn.gonzales@gmail.com>
2018-04-10 12:27:08 -0700

    add test to display or and not display .visuallyhidded if key is 0 and not 0

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import Feedback from './feedback';
 
 describe('<Feedback />', () => {
 	it('Renders without crashing', () => {
        shallow(<Feedback />);
+    });
+
+    it.only('Display .visuallyhidden if key is not 0', () => {
+       const wrapper = mount(<Feedback guessCount={1} feedback='ok'/>);
+       expect (wrapper.find('.visuallyhidden').length).toEqual(1);
+       expect (wrapper.props().feedback).toEqual('ok');
+    });
+
+    it.only('Not display .visuallyhidden if key is 0', () => {
+       const wrapper = mount(<Feedback guessCount={0} feedback='ok'/>);
+       expect (wrapper.find('.visuallyhidden').length).toEqual(0);
+       expect (wrapper.props().feedback).toEqual('ok');
     });
 }); //describe('<Game/>')
