b9441c9f2198059976e9bd78d3418fecfac83201
Benilyn <benilyn.gonzales@gmail.com>
2018-04-10 12:29:00 -0700

    add test to render FeedBack with props and GuessForm

--- src/components/guess-section.test.js
+++ src/components/guess-section.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 
 import GuessSection from './guess-section';
 
@@ -7,4 +7,18 @@ describe('<GuessSection />', () => {
 	it('Renders without crashing', () => {
        shallow(<GuessSection />);
     });
+
+    it('Renders Feedback', () => {
+    	const wrapper = mount(<GuessSection feedback="ok" guessCount={3}/>);
+    	expect (wrapper.find('Feedback').length).toEqual(1);
+    	console.log(wrapper.props());
+    	expect (wrapper.props().feedback).toEqual("ok");
+    	expect (wrapper.props().guessCount).toEqual(3);
+    });
+
+    it('Renders GuessForm', () => {
+    	const wrapper = shallow(<GuessSection />);
+    	expect (wrapper.find('GuessForm').length).toEqual(1);
+    });
+
 }); //describe('<Game/>')
