8faedab3b00bbeb80eb596d4c5dea0253768a4ad
Benilyn <benilyn.gonzales@gmail.com>
2018-04-10 12:37:06 -0700

    corrected test to trigger .onMakeGuess on submit

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -7,18 +7,17 @@ describe('<GuessForm />', () => {
 	it('Renders without crashing', () => {
        shallow(<GuessForm />);
     });
-{/*
-    it('should fire the onSubmit callback when the form is submitted', () => {
+
+    it.only('should call onMakeGuess function when submit is clicked', () => {
     	const callback = jest.fn();
 	    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
-	    const value = "88";
-	    wrapper.instance().setEditing(true);
-	    wrapper.update();
-	    wrapper.find('#userGuess').instance().value = value;
+        const value = "88";
+        wrapper.find('input[type="number"]').instance().value = value;
 	    wrapper.simulate('submit');
-	    expect(callback).toHaveBeenCalledWith(value);
+        expect(callback).toHaveBeenCalledWith(value);
+    
     });
-
+{/*
     it('Should not fire onSubmit callback when form is empty', () => {
         const callback = jest.fn();
         const wrapper = mount(<GuessForm onMakeGuess={callback} />);
