dfb2010aaaa434db89ea0315d647158d945fc6d0
Benilyn <benilyn.gonzales@gmail.com>
2018-04-10 12:33:53 -0700

    added another value to guess

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -9,7 +9,7 @@ describe('<GuessList />', () => {
     });
 
     it('Renders GuessList', () => {
-    	const guesses = [3];
+    	const guesses = [3, 8];
     	const items = guesses.map((guess, index) => (
 		    <li key={index}>
 		      {guess}
@@ -17,5 +17,6 @@ describe('<GuessList />', () => {
 		    ));
     	const wrapper = shallow(<GuessList />);
     	expect (wrapper.contains(<ul id='guessList'> {items} </ul>)).not.toEqual(0);
+       
     });
-}); //describe('<Game/>')
+}); {/*describe('<Game/>')*/}
